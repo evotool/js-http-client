3c1da1f4581bb25f25420188f943b3cffcb3c059
Shamil Sultanov <shamil.amirovich@gmail.com>
2020-12-29 19:43:06 +0300

    tools/build: update

--- tools/build.js
+++ tools/build.js
@@ -11,14 +11,14 @@ delete packageJson.devDependencies;
 
 writeFileSync(resolve(__dirname, '../dist/package.json'), JSON.stringify(packageJson, null, '\t'));
 
+const jsTsFiles = sync(resolve(__dirname, '../dist/**/*.{js,ts}'));
+
+for (const f of jsTsFiles) {
+	writeFileSync(f, readFileSync(f, 'utf-8').replace(/ {4}/g, '\t'));
+}
+
 const copyingFiles = ['LICENSE', 'README.md'];
 
 for (const cf of copyingFiles) {
 	copyFileSync(resolve(__dirname, `../${cf}`), resolve(__dirname, `../dist/${cf}`));
-}
-
-const files = sync(resolve(__dirname, '../dist/**/*.{js,ts}'));
-
-for (const f of files) {
-	writeFileSync(f, readFileSync(f, 'utf-8').replace(/ {4}/g, '\t'));
 }
